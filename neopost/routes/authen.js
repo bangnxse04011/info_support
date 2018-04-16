28305b7bf5145dbda71415dfb50e3f1ed729a573
bangnx1 <bangnx1@fsoft.com.vn>
2018-04-17 00:17:58 +0700

    update code login

--- neopost/routes/authen.js
+++ neopost/routes/authen.js
@@ -2,6 +2,7 @@ const express = require('express');
 const login = require('../public/javascripts/Info_DAO/account_dao');
 const message = require('../public/javascripts/common/message_common');
 const page = require('../public/javascripts/common/page_common');
+const db_tabale_account = require('../public/javascripts/DAO/db_table_account');
 const router = express.Router();
 
 /**
@@ -19,16 +20,18 @@ router.post('/', function (req, res, next) {
         res.render(page.page_login, { mess: message.MSG_ACC_2 });
     }
     // Check authen by username and password
-    login.authen_login(user_name, pass_word).then(
-        (resolve) => {
-            console.log(resolve + "-------------------------------");
-        },
-
-        (reject) => {
-            console.log("--------------------------");
-            console.log(reject);
+    db_tabale_account.findOne({
+        where: {
+            user_name: user_name,
+            pass_word: pass_word
         }
-    );
+    }).then(info => {
+        console.log(info);
+        // res.render(page.page_detail, { info: info });
+    }).catch(function (err) {
+        console.log(err);
+        res.render('error');
+    });
 });
 
 module.exports = router;
