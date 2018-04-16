4df376300e2c9042cd6f4deb5108958318f9e509
bangnx1 <bangnx1@fsoft.com.vn>
2018-04-16 23:40:57 +0700

    update code

--- neopost/routes/info_support.js
+++ neopost/routes/info_support.js
@@ -1,7 +1,7 @@
 var express = require('express');
+var router = express.Router();
 const info_support = require('../public/javascripts/Info_DAO/info_support');
 const db_tabale_info_support = require('../public/javascripts/DAO/db_table_info_support');
-var router = express.Router();
 const page = require('../public/javascripts/common/page_common');
 
 /* GET users listing. */
@@ -14,24 +14,22 @@ router.get('/', function (req, res, next) {
 /**
  * Find object by id
  */
-router.get('/:id', function (req, res, next) {
+router.get('/details/:id', function (req, res, next) {
     let id_info = req.params['id'];
     console.log(id_info);
-    // // if (id_info == null || isNaN(id_info) || id_info == '' || id_info == "") {
-    // //     res.render(page.page_error);
-    // // }
-    // db_tabale_info_support.findOne({
-    //     where: {
-    //         id: id_info
-    //     }
-    // }).then(info => {
-    //     console.log(info.dataValues);
-    //     // res.render(page.page_detail);
-    // }).catch(function (err) {
-    //     console.log(err);
-    //     // res.render('error');
-    // });
-    res.render('details'); 
+    if (id_info == null || isNaN(id_info) || id_info == '' || id_info == "") {
+        res.render(page.page_error);
+    }
+    db_tabale_info_support.findOne({
+        where: {
+            id: id_info
+        }
+    }).then(info => {
+        res.render(page.page_detail, { info: info });
+    }).catch(function (err) {
+        console.log(err);
+        res.render('error');
+    });
 });
 
 module.exports = router;
